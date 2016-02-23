7b61911e70a61cbcb31375ff30d3e8f4c0e461fd
Anna Migas <amjmigas@gmail.com>
2016-02-23 12:40:13 +0100

    Fix build task, create separate css for library only, clean things up

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,42 @@
 var gulp = require('gulp');
+var del = require('del');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
 var runSequence = require('run-sequence');
 
-gulp.task('sass', function() {
+gulp.task('sass-demo', function() {
   return gulp.src('app/scss/styles.scss')
   .pipe(sass({outputStyle: 'expanded'}))
   .pipe(autoprefixer('last 2 versions'))
-  .pipe(gulp.dest('app/css'))
+  .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('fonts', function() {
-  return gulp.src('app/fonts/**/*')
-  .pipe(gulp.dest('dist/fonts'))
+gulp.task('sass-build', function() {
+  return gulp.src('app/scss/repaintless.scss')
+  .pipe(sass({outputStyle: 'expanded'}))
+  .pipe(autoprefixer('last 2 versions'))
+  .pipe(gulp.dest('repaintless-css'))
+  .pipe(cssnano())
+  .pipe(rename("repaintless.min.css"))
+  .pipe(gulp.dest('repaintless-css'));
 });
 
-gulp.task('clean:dist', function() {
-  return del.sync('dist');
+gulp.task('clean:repaintless-css', function() {
+  return del.sync('repaintless-css');
 });
 
 gulp.task('watch', function() {
-  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/scss/**/*.scss', ['sass-demo']);
 });
 
 gulp.task('default', function(callback) {
-  runSequence(['sass', 'watch'],
+  runSequence(['sass-demo', 'watch'],
     callback
-  )
-})
+  );
+});
 
 gulp.task('build', function(callback) {
-  runSequence('clean:dist', ['sass', 'fonts'], callback);
+  runSequence('clean:repaintless-css', 'sass-build', callback);
 });

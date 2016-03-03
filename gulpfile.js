ffd4778aa9baa90ce5124cfd12f0d26d28f1710c
Anna Migas <amjmigas@gmail.com>
2016-03-03 16:45:20 +0100

    Major structural changes, preparing for bower and npm

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,18 @@ var cssnano = require('gulp-cssnano');
 var runSequence = require('run-sequence');
 
 gulp.task('sass-demo', function() {
-  return gulp.src('app/scss/styles.scss')
+  return gulp.src('demo/scss/styles.scss')
   .pipe(sass({outputStyle: 'expanded'}))
   .pipe(autoprefixer('last 2 versions'))
-  .pipe(gulp.dest('app/css'));
+  .pipe(gulp.dest('demo/css'));
 });
 
 gulp.task('sass-build', function() {
-  return gulp.src('app/scss/repaintless.scss')
+  return gulp.src('repaintless-scss/repaintless.scss')
   .pipe(sass({outputStyle: 'expanded'}))
   .pipe(autoprefixer('last 2 versions'))
   .pipe(gulp.dest('repaintless-css'))
+  .pipe(gulp.dest('demo/css'))
   .pipe(cssnano())
   .pipe(rename("repaintless.min.css"))
   .pipe(gulp.dest('repaintless-css'));
@@ -27,16 +28,26 @@ gulp.task('clean:repaintless-css', function() {
   return del.sync('repaintless-css');
 });
 
-gulp.task('watch', function() {
-  gulp.watch('app/scss/**/*.scss', ['sass-demo']);
+gulp.task('watch-animations', function() {
+  gulp.watch('repaintless-scss/**/*.scss', ['sass-build']);
 });
 
 gulp.task('default', function(callback) {
-  runSequence(['sass-demo', 'watch'],
+  runSequence(['sass-build', 'watch-animations'],
     callback
   );
 });
 
 gulp.task('build', function(callback) {
   runSequence('clean:repaintless-css', 'sass-build', callback);
+});
+
+gulp.task('watch-demo', function() {
+  gulp.watch('demo/scss/**/*.scss', ['sass-demo']);
+});
+
+gulp.task('demo', ['watch-animations'], function(callback) {
+  runSequence(['sass-demo', 'watch-demo'],
+    callback
+  );
 });
